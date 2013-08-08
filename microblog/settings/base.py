168731631556670e908e52309f713b703e3e29cf
Josh Caldwell <jpcaldwell@gmail.com>
2013-08-08 22:40:50 +0000

    added admin interface

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -78,7 +78,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    root("assets")
+    root("assets"),
 )
 
 # List of finder classes that know how to find static files in
@@ -118,7 +118,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    root("templates")
+    root("templates"),
 )
 
 DJANGO_APPS = (
